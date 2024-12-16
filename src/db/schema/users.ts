b3b201759f1263b622c5a0730e384200743e5218
xgerza <xgerza@fi.muni.cz>
2024-12-16 17:51:41 +0100

    users table scheme update

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -5,7 +5,8 @@ import { catNames } from '@/db/schema/catNames';
 
 export const users = sqliteTable('users', {
 	id: integer('id').primaryKey({ autoIncrement: true }),
-	name: text('name').notNull()
+	name: text('name').notNull(),
+	email: text('email').notNull()
 });
 
 export const usersRelations = relations(users, ({ many }) => ({
