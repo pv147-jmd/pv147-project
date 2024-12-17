0d76baa9fa908e3fc3b440f4319b629c103981e7
Josef Melkus <josef.melkus@bringauto.com>
2024-12-17 18:27:40 +0100

    delete experimental

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	experimental: {
-		turbo: false, // Disable Turbopack and revert to Webpack
-	  },
 	productionBrowserSourceMaps: true,
 	/* config options here */
 	images: {
