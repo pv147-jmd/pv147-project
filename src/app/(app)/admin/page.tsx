f66b24282c637d456b2633b2ee747759a2d5d340
Josef Melkus <josef.melkus@bringauto.com>
2024-12-17 15:09:15 +0100

    Adding cat names more responsive

--- src/app/(app)/admin/page.tsx
+++ src/app/(app)/admin/page.tsx
@@ -3,8 +3,11 @@
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
+import { toast } from 'sonner';
 
 import { uploadNames } from '@/app/(app)/admin/InsertNamesFromFile';
+import { SubmitButton } from '@/components/form/submit-button';
 
 const formSchema = z.object({
 	name: z.string()
@@ -13,25 +16,33 @@ const formSchema = z.object({
 type FormSchema = z.infer<typeof formSchema>;
 
 const AdminPage = () => {
-	// const upload = async () => {
-	// 	await uploadNames(names);
-	// };
+	// TODO verify admin
+	const [isUploading, setIsUploading] = useState<boolean>(false);
+
 	const form = useForm<FormSchema>({
 		resolver: zodResolver(formSchema)
 	});
 
 	const onSubmit = async (values: FormSchema) => {
-		await uploadNames(values.name);
-		form.reset();
+		setIsUploading(true);
+		try {
+			await uploadNames(values.name);
+			form.reset();
+			toast.success('Names uploaded');
+		} finally {
+			setIsUploading(false);
+		}
 	};
 	return (
 		<form onSubmit={form.handleSubmit(onSubmit)}>
 			<textarea
+				className="p-2"
 				{...form.register('name')}
 				placeholder="Enter names, one per line"
+				rows={10}
+				cols={50}
 			/>
-			{/*<input {...form.register('name')} type="text" />*/}
-			<button type="submit">Submit</button>
+			<SubmitButton isLoading={isUploading} />
 		</form>
 	);
 };
