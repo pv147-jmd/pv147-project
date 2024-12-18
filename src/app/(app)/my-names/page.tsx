aa2a203ddb9765456cc189fc5ac1484f0c640c33
Josef Melkus <josef.melkus@bringauto.com>
2024-12-18 18:18:06 +0100

    loading

--- src/app/(app)/my-names/page.tsx
+++ src/app/(app)/my-names/page.tsx
@@ -39,7 +39,11 @@ const MyCatNames = () => {
 	}, [user?.id]);
 
 	if (loading) {
-		return <div>Loading...</div>;
+		return (
+			<div className="flex items-center justify-center">
+				<div className="h-12 w-12 animate-spin rounded-full border-t-4 border-solid border-blue-500 border-opacity-50" />
+			</div>
+		);
 	}
 
 	return (
