4d282a3cda8cc7f1fe5aed338cfce431211faee4
Josef Melkus <josef.melkus@bringauto.com>
2024-12-18 18:23:50 +0100

    loader

--- src/app/(app)/my-names/page.tsx
+++ src/app/(app)/my-names/page.tsx
@@ -40,9 +40,12 @@ const MyCatNames = () => {
 
 	if (loading) {
 		return (
-			<div className="flex items-center justify-center">
-				<div className="h-12 w-12 animate-spin rounded-full border-t-4 border-solid border-blue-500 border-opacity-50" />
-			</div>
+			<>
+				<h1 className="text-center text-3xl">Moje kočičky</h1>
+				<div className="flex items-center justify-center">
+					<div className="h-12 w-12 animate-spin rounded-full border-t-4 border-solid border-blue-500 border-opacity-50" />
+				</div>
+			</>
 		);
 	}
 
